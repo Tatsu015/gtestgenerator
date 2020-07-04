2bf77bb292b48e21ae80d340fa37e38dfddfd90c
Tatsu015 <tatsumi.soichiro.com>
2020-07-04 22:09:48 +0900

    modify lizard parser

--- src/lizard_parser.py
+++ src/lizard_parser.py
@@ -2,13 +2,14 @@
 
 import re
 import os
+import json
 
 def __class_and_function(data):
     if '::' in data:
         tmp = data.split('::')
         return {'class':tmp[0], 'function':tmp[1]}
     else:
-        return {'class':None, 'function':data}
+        return {'class':'', 'function':data}
 
 def __basename(path):
     tmp = os.path.basename(path)
@@ -49,12 +50,66 @@ def parse(file_path):
     f = open(file_path, 'r')
     table_data = __trimmed(f.read())
 
-    ls = []
+    lines = []
     for line in table_data.splitlines():
         obj = __line_to_object(line)
-        print(obj)
-        ls.append(obj)
+        lines.append(obj)
 
-    return ls
+    return lines
 
-parse('test.lizard')
+class SourceCodeManager:
+    def __init__(self):
+        self.__files = []
+
+    def to_json(self, line):
+        #　use file basename insted class name
+        # because function (not method) has no class name
+        class_name = line['class']
+        if class_name == '':
+            class_name = line['basename']
+        func_name = line['function']
+
+        filepathes = [d.get('filepath') for d in self.__files]
+        if line['path'] in filepathes:
+            classes = [d.get('testdata').get('classes') for d in self.__files][0]
+            hit_classes = [x for x in classes if x['classname'] == class_name]
+            if len(hit_classes) != 0:
+                cls = hit_classes[0]
+                fncs = [d.get('func') for d in classes][0]
+                fnc = [x for x in fncs if x['funcname'] == func_name]
+
+                # append suffix index to same function name,
+                # because override function name duplicatable.
+                index = 1
+                str_index = ''
+                while fnc:
+                    index = index + 1
+                    str_index = str(index)
+                    fnc = [x for x in fncs if x['funcname'] == (func_name + str_index)]
+
+                cls['func'].append({'funcname':(func_name + str_index),'body':''})
+            else:
+                classes.append({'classname':class_name,'func':[{'funcname':line['function'],'body':''}]})
+        else:
+            self.__files.append({
+                'filepath':line['path'],
+                'testdata':{
+                    'includepath':[
+                        {'filepath':line['basename']+'.h'}
+                    ],
+                    'classes':[
+                        {'classname':class_name,'func':[{'funcname':line['function'],'body':''}
+                        ]}
+                    ]
+                }
+            })
+
+    def dump(self):
+        print(json.dumps(self.__files,indent=1))
+
+lines = parse('test/test.lizard')
+m = SourceCodeManager()
+for line in lines:
+    m.to_json(line)
+
+m.dump()
