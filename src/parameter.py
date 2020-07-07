6b88b3a81bfc1d4047e4745173d8c938c91f1f02
Tatsu015 <tatsumi.soichiro.com>
2020-07-08 00:34:55 +0900

    add argument parameter

--- src/parameter.py
+++ src/parameter.py
@@ -6,13 +6,13 @@ import argparse
 __parameter = {}
 
 def load_args():
-    parser = argparse.ArgumentParser(description='xxx')
-    parser.add_argument('--ccn', type=int,default=0)
-    parser.add_argument('--nloc', type=int,default=0)
-    parser.add_argument('-s','--source', type=str,default='.')
-    parser.add_argument('-d','--destination', type=str,default='testcode')
-    parser.add_argument('--template', type=str,default='./conf/main.template')
-    parser.add_argument('-f','--file', type=str,default='')
+    parser = argparse.ArgumentParser(description='Automatically generate google test skeleton from c++ source code')
+    parser.add_argument('--ccn', type=int,default=0,help='Generate skeletons only for functions greater than the specified cyclomatic complexity')
+    parser.add_argument('--nloc', type=int,default=0,help='Generate skeletons only for functions greater than the specified line count')
+    parser.add_argument('-s','--source', type=str,default='.',help='Specify the source code directory for skeleton generation')
+    parser.add_argument('-d','--destination', type=str,default='testcode',help='Specify the skeleton output destination directory')
+    parser.add_argument('--template', type=str,default='./conf/main.template',help='Specify the skeleton template file')
+    parser.add_argument('-f','--file', type=str,default='',help='Load parameters from specify file')
 
     args = parser.parse_args()
 
