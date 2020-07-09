aa3cab65192e1b08781f2c11b41013c1ba4e9ee8
Tatsu015 <tatsumi.soichiro.com>
2020-07-10 00:53:01 +0900

    add test code parser for merge already exist test code

--- src/test_code_parser.py
+++ src/test_code_parser.py
@@ -1,1 +1,105 @@
 # -*- coding: utf-8 -*-
+
+import os
+import re
+
+def parse(dirpath):
+    pass
+
+def parse_file(filepath):
+    f = open(filepath, 'r')
+    d = f.read()
+    root_obj = {}
+
+    root_obj['dstfilepath'] = filepath
+    root_obj['testdata'] = parse_testdata(d)
+
+    return root_obj
+
+def parse_testdata(testdata):
+    testdata_obj = {}
+    testdata_obj['includepath'] = parse_includepath(testdata)
+    testdata_obj['classes'] = parse_classes(testdata)
+
+    return testdata_obj
+
+def parse_includepath(testdata):
+    includefiles_obj = []
+    for includefile in __extract_includefiles(testdata):
+        filepath_obj = {'filepath':includefile}
+        includefiles_obj.append(filepath_obj)
+
+    return includefiles_obj
+
+def parse_classes(testdata):
+    classes_obj = []
+    for testfixture in __extract_testfixtures(testdata):
+        classes_obj.append(parse_class(testfixture))
+
+    return classes_obj
+
+def parse_class(testfixture):
+    class_obj = {}
+    class_obj['classname'] = __extract_class_name(testfixture)
+    class_obj['fixturebody'] = __extract_testfixture_class_body(testfixture)
+    class_obj['func'] = parse_functions(testfixture)
+
+    return class_obj
+
+def parse_functions(testfixture):
+    func_objs = []
+    for testcase in __extract_testcases(testfixture):
+        func_obj = {
+            'funcname':__extract_test_name(testcase),
+            'body':__extract_test_body(testcase)
+        }
+        func_objs.append(func_obj)
+
+    return func_objs
+
+def __extract_includefiles(testdata):
+    includefiles = []
+    for line in testdata.splitlines():
+        if '#include' in line:
+            tmp = line.replace('#include','').strip()
+            includefile = tmp.replace('"','').replace('<', '').replace('>', '')
+            if 'gtest.h' not in includefile and 'gmock.h' not in includefile:
+                includefiles.append(includefile)
+    return includefiles
+
+def __extract_testfixtures(testdata):
+    tmp = re.split(r'class\s',testdata)
+    return tmp[1:]
+
+def __extract_testfixture_class(testfixture):
+    tmp = re.split('TEST_F|TEST', testfixture)
+    return tmp[0]
+
+def __extract_class_name(testfixture):
+    tmp = testfixture.split(':')[0]
+    tmp = tmp.strip().replace('_test', '')
+    return tmp
+
+def __extract_testfixture_class_body(testfixture):
+    re_body = re.compile(r'(?<=\{).+(?=\})', flags=(re.MULTILINE | re.DOTALL))
+    match_body = re_body.search(testfixture)
+    return match_body.group()
+
+def __extract_testcases(testfixture):
+    tmp = re.split('TEST_F|TEST', testfixture)
+    return tmp[1:]
+
+def __extract_test_name(testcase):
+    re_name = re.compile(r'(?<=\()[^\(\)]+(?=\))')
+    match_names = re_name.search(testcase)
+    return match_names.group().split(',')[1]
+
+def __extract_test_body(testcase):
+    re_body = re.compile(r'(?<=\{).+(?=\})', flags=(re.MULTILINE | re.DOTALL))
+    match_body = re_body.search(testcase)
+    return match_body.group()
+
+# s = '/home/southeast/workspace/Python/gtestgenerator/test/stringutil_test.cpp'
+# fs = parse_file(s)
+# print(fs)
+
--- test/stringutil_test.cpp
+++ test/stringutil_test.cpp
@@ -5,7 +5,7 @@
 #include "../../src/utility/stringutil.h"
 using namespace ::testing;
 
-class Global_stringutil_test : public ::testing::Test {
+class stringutil_test : public ::testing::Test {
 protected:
   virtual void SetUp() {
     // FIX ME!
@@ -15,122 +15,45 @@ protected:
   }
 };
 
-TEST(Global_stringutil_test, stob) {
+TEST_F(stringutil_test, stob) {
   // FIX ME!
 }
 
-TEST(Global_stringutil_test, btos) {
+TEST_F(stringutil_test, btos) {
   // FIX ME!
 }
 
-TEST(Global_stringutil_test, nextVersasBracket) {
+TEST_F(stringutil_test, nextVersasBracket) {
   QString t = "{{a}}";
   ASSERT_EQ(nextVersasBracket(t, 0), 5);
   ASSERT_EQ(nextVersasBracket(t, 1), 4);
+  if(aaa){
+    testtest
+  }
 }
 
-TEST(Global_stringutil_test, indent) {
+class HeaderFile_test : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    // FIX ME!
+  }
+  virtual void TearDown() {
+    // FIX ME!
+  }
+};
+
+TEST(HeaderFile_test, HeaderFile) {
   // FIX ME!
 }
 
-TEST(Global_stringutil_test, downIndent) {
-  QString t = "{\n"
-              "  {\n"
-              "    a\n"
-              "  }\n"
-              "}\n";
-
-  QString e1 = "{\n"
-               "{\n"
-               "  a\n"
-               "}\n"
-               "}\n";
-  t = downIndent(t, 2);
-  ASSERT_STREQ(t.toStdString().c_str(), e1.toStdString().c_str());
-
-  QString e2 = "{\n"
-               "{\n"
-               "a\n"
-               "}\n"
-               "}\n";
-  t = downIndent(t, 2);
-  ASSERT_STREQ(t.toStdString().c_str(), e2.toStdString().c_str());
+TEST(HeaderFile_test, parse) {
+  // FIX ME!
 }
 
-TEST(Global_stringutil_test, upIndent) {
-  //  QString d = "{\n"
-  //              "{\n"
-  //              "a\n"
-  //              "}\n"
-  //              "}\n";
-
-  //  QString dexp1 = "  {\n"
-  //                  "  {\n"
-  //                  "  a\n"
-  //                  "  }\n"
-  //                  "  }\n";
-  //  d = upIndent(d, 2);
-  //  ASSERT_STREQ(d.toStdString().c_str(), dexp1.toStdString().c_str());
+TEST(HeaderFile_test, removeCComment) {
+  // FIX ME!
 }
 
-TEST(Global_stringutil_test, stripBlacket) {
-  QString t = "{\n"
-              "  {\n"
-              "    a\n"
-              "  }\n"
-              "}\n";
-
-  QString e1 = "  {\n"
-               "    a\n"
-               "  }";
-  QString t1 = stripBlacket(t, false);
-  ASSERT_STREQ(t1.toStdString().c_str(), e1.toStdString().c_str());
-
-  QString e2 = "    a";
-  t1 = stripBlacket(t1, false);
-  ASSERT_STREQ(t1.toStdString().c_str(), e2.toStdString().c_str());
-
-  QString e3 = "{\n"
-               "  a\n"
-               "}";
-  QString t2 = stripBlacket(t);
-  ASSERT_STREQ(t2.toStdString().c_str(), e3.toStdString().c_str());
-
-  QString e4 = "a";
-  t2 = stripBlacket(t2);
-  ASSERT_STREQ(t2.toStdString().c_str(), e4.toStdString().c_str());
-}
-
-TEST(Global_stringutil_test, removeContinuityBlank) {
-  QString t = "    a   ";
-  QString e = " a ";
-  ASSERT_STREQ(removeContinuityBlank(t).toStdString().c_str(), e.toStdString().c_str());
-}
-
-TEST(Global_stringutil_test, systemIncludeCode) {
-  QString t = systemIncludeCode("a");
-  QString e = "#include <a>";
-  ASSERT_STREQ(t.toStdString().c_str(), e.toStdString().c_str());
-}
-
-TEST(Global_stringutil_test, localIncludeCode) {
-  QString t = localIncludeCode("a");
-  QString e = "#include \"a\"";
-  ASSERT_STREQ(t.toStdString().c_str(), e.toStdString().c_str());
-}
-
-TEST(Global_stringutil_test, defineCode) {
-  QString t = defineCode("a", "b");
-  QString e = "#define a b";
-  ASSERT_STREQ(t.toStdString().c_str(), e.toStdString().c_str());
-}
-
-TEST(Global_stringutil_test, usingNamespaceCode) {
-  QString t = usingNamespaceCode("a");
-  QString e = "using namespace a;";
-  ASSERT_STREQ(t.toStdString().c_str(), e.toStdString().c_str());
-}
-
-TEST(Global_stringutil_test, removeLastReturnDuplicate) {
+TEST(HeaderFile_test, removeCppComment) {
   // FIX ME!
 }
