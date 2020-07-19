3011822de0224befc570e1511d77d062988e8713
Tatsu015 <tatsumi.soichiro.com>
2020-07-19 23:09:03 +0900

    add config file generate

--- src/parameter.py
+++ src/parameter.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import json
 import argparse
 
@@ -7,13 +8,57 @@ __parameter = {}
 
 def load_args():
     parser = argparse.ArgumentParser(description='Automatically generate google test skeleton from c++ source code')
-    parser.add_argument('--ccn', type=int,default=0,help='Generate skeletons only for functions greater than the specified cyclomatic complexity')
-    parser.add_argument('--nloc', type=int,default=0,help='Generate skeletons only for functions greater than the specified line count')
-    parser.add_argument('-s','--source', type=str,default='.',help='Specify the source code directory for skeleton generation')
-    parser.add_argument('-d','--destination', type=str,default='testcode',help='Specify the skeleton output destination directory')
-    parser.add_argument('--template', type=str,default='./conf/testcode.template',help='Specify the skeleton template file')
-    parser.add_argument('-f','--file', type=str,default='',help='Load parameters from specify file')
-    parser.add_argument('--exclude', type=str,default='',nargs='*',help='Exclude spacify source file name')
+
+    parser.add_argument(
+        '--ccn',
+        type=int,
+        default=0,
+        help='Generate skeletons only for functions greater than the specified cyclomatic complexity')
+
+    parser.add_argument(
+        '--nloc',
+        type=int,
+        default=0,
+        help='Generate skeletons only for functions greater than the specified line count')
+
+    parser.add_argument(
+        '-s',
+        '--source',
+        type=str,
+        default='.',
+        help='Specify the source code directory for skeleton generation')
+
+    parser.add_argument(
+        '-d',
+        '--destination',
+        type=str,
+        default='testcode',
+        help='Specify the skeleton output destination directory')
+
+    parser.add_argument(
+        '--template',
+        type=str,
+        default=os.path.dirname(__file__) + '../conf/testcode.template',
+        help='Specify the skeleton template file')
+
+    parser.add_argument(
+        '--config',
+        type=str,
+        default='',
+        help='Load configuration from specify file')
+
+    parser.add_argument(
+        '--exclude',
+        type=str,
+        default='',
+        nargs='*',
+        help='Exclude spacify source file name')
+
+    parser.add_argument(
+        '-i',
+        '--init',
+        help='Generate google test generator config file',
+        action='store_true')
 
     args = parser.parse_args()
 
@@ -23,6 +68,19 @@ def load_args():
     __parameter['destination'] = args.destination
     __parameter['template'] = args.template
     __parameter['exclude'] = args.exclude
+
+    if args.init:
+        f = open('.gtgconfig','w')
+        parameter = {
+            'ccn':__parameter['ccn'],
+            'nloc':__parameter['nloc'],
+            'source':__parameter['source'],
+            'destination':__parameter['destination'],
+            'template':__parameter['template'],
+            'exclude':__parameter['exclude']
+        }
+        json.dump(parameter, f, indent=4)
+        exit(0)
 
 def get(key):
     return __parameter[key]
