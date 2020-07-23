9bd685e8c194b936c4872ad85ff7660c78e64f7f
Tatsu015 <tatsumi.soichiro.com>
2020-07-23 13:57:42 +0900

    create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="gtestgenerator",
+    version='1.0.0',
+    description='C++ google test skeleton generator',
+    author='s_tatsu',
+    author_email='',
+    url='https://bitbucket.org/s_tatsu/gtestgenerator.git',
+    packages=find_packages(),
+        entry_points="""
+      [console_scripts]
+      gtestgenerator = gtestgenerator.gtestgenerator:main
+    """,
+)
